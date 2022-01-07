5ca5822e4c7d3eef85d1331689a6fde0faf02201
pavloteyfel <pavloteyfel@gmail.com>
2022-01-07 14:08:10 +0100

    chore: Add allowed hosts

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "pt-django-app.eu-gb.cf.appdomain.cloud",
+    ]
 
 
 # Application definition
