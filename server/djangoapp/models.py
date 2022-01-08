52c98216085cfbb03e0aadaccc26d74af1af877d
pavloteyfel <pavloteyfel@gmail.com>
2022-01-08 21:01:51 +0100

    feature: Add models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,27 +1,60 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.utils.timezone import now
+from dataclasses import dataclass
+from datetime import datetime
 
 
-# Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=120, null=False)
+    description = models.TextField(null=False)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return f"{self.name}"
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarModel(models.Model):
+    class CarModelType(models.TextChoices):
+        SUV = "SUV"
+        WAGON = "Wagon"
+        SEDAN = "Sedan"
+        COUPE = "Coupe"
+        MINIVAN = "Minivan"
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120, null=False)
+    dealerid = models.IntegerField(null=False)
+    year = models.DateField(null=False)
+    type = models.CharField(
+        max_length=10, choices=CarModelType.choices, default=CarModelType.SEDAN
+    )
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"
 
 
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+@dataclass
+class CarDealer:
+    address: str = None
+    city: str = None
+    full_name: str = None
+    id: int = None
+    lat: float = None
+    long: float = None
+    short_name: str = None
+    st: str = None
+    state: str = None
+    zip: str = None
+
+
+@dataclass
+class DearlerReview:
+    id: int = None
+    car_make: str = None
+    car_model: str = None
+    car_year: int = None
+    dealership: int = None
+    name: str = None
+    purchase: bool = None
+    purchase_date: datetime = None
+    review: str = None
